9156f4cc8b61de4b72c3c88aa81a49c0f39c98c5
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2017-02-27 23:55:39 -0800

    Add more DB Schema

--- api-server/db/schema.rb
+++ api-server/db/schema.rb
@@ -10,7 +10,85 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161214031618) do
+ActiveRecord::Schema.define(version: 20170228073509) do
+
+  create_table "contest_votes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "voted_for"
+    t.integer  "contest_id"
+    t.string   "ip_and_useragent"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "contests", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.text     "rules"
+    t.integer  "credit_reward"
+    t.datetime "start"
+    t.datetime "end"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "credit_purchases", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "amount"
+    t.string   "payment_method"
+    t.string   "ip_and_useragent"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "credit_transfers", force: :cascade do |t|
+    t.integer  "from_user_id"
+    t.integer  "to_user_id"
+    t.string   "type"
+    t.integer  "amount"
+    t.integer  "dev_id"
+    t.integer  "dev_share_amount"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  create_table "game_logs", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "private_messages", force: :cascade do |t|
+    t.integer  "from_user_id"
+    t.integer  "to_user_id"
+    t.text     "message",      limit: 1024
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
+
+  create_table "user_blocks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "marked_as_blocked"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
+
+  create_table "user_favorites", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "marked_as_favorite"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+  end
+
+  create_table "user_files", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "file_file_name"
+    t.string   "file_content_type"
+    t.integer  "file_file_size"
+    t.datetime "file_updated_at"
+    t.boolean  "is_public"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
@@ -20,6 +98,7 @@ ActiveRecord::Schema.define(version: 20161214031618) do
     t.string   "middle_name"
     t.string   "last_name"
     t.datetime "birthdate"
+    t.string   "account_status"
     t.boolean  "admin",                                    default: false
     t.boolean  "broadcaster",                              default: false
     t.boolean  "developer",                                default: false
@@ -27,6 +106,7 @@ ActiveRecord::Schema.define(version: 20161214031618) do
     t.string   "address_line_1"
     t.string   "address_line_2"
     t.string   "address_line_3"
+    t.string   "timezone"
     t.boolean  "dark_mode",                                default: false
     t.boolean  "send_email_favorites_online",              default: false
     t.boolean  "send_email_site_news",                     default: false
@@ -36,7 +116,6 @@ ActiveRecord::Schema.define(version: 20161214031618) do
     t.integer  "profile_age",                 limit: 3
     t.string   "profile_location",            limit: 32
     t.string   "profile_languages",           limit: 32
-    t.text     "profile_platforms",           limit: 2048
     t.datetime "created_at",                                               null: false
     t.datetime "updated_at",                                               null: false
   end
