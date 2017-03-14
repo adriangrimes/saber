9d958793c6af1d9c8e984fc67d68c57f83f89b51
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2017-03-13 20:34:00 -0700

    Remove login-state injections

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-  loginState: Ember.inject.service('login-state'),
-  
   model () {
     //eventually have some actual auth here. probably some jazz with login pages
     //return admin user
