d571f8b718596395b7ef82b03ff9a86963bdb680
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2018-04-24 23:09:56 -0700

    Update Ruby to 2.4.4, Rails to 5.2.0.

--- api-server/config/application.rb
+++ api-server/config/application.rb
@@ -18,6 +18,9 @@ Bundler.require(*Rails.groups)
 
 module ApiServer
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -35,7 +38,7 @@ module ApiServer
       DeviseController.respond_to :jsonapi
     end
 
-    config.middleware.insert_before 0, "Rack::Cors" do
+    config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :options]
--- api-server/config/boot.rb
+++ api-server/config/boot.rb
@@ -3,3 +3,4 @@ ENV['HOST'] = '0.0.0.0'
 ENV['PORT'] = '3000'
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
