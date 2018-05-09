98c079e005f690d5f119387c592306c2591a930e
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2018-05-08 19:44:34 -0700

    Update /p template for use on mobile

--- app/controllers/p.js
+++ app/controllers/p.js
@@ -2,11 +2,139 @@ import Controller from '@ember/controller';
 
 export default Controller.extend({
 
+  tipToggled:false,
   actions:{
     toggleFav(user){
       this.toggleProperty('isFavorite');
     },
+    confirmTip(){
+        // hide tip forms
+          $('[id=tip-form]').removeClass('d-flex');
+          $('[id=tip-form]').addClass('d-none');
+          $('[id=simple-tip-form]').removeClass('d-flex');
+          $('[id=simple-tip-form]').addClass('d-none');
+        // Show Tip Confirm Form
+
+          $('[id=confirm-tip-form]').removeClass('d-none');
+          $('[id=confirm-tip-form]').addClass('d-flex');
+
+    },
+    cancelTip(){
+
+      //Hide Confirm Tip
+
+      $('[id=confirm-tip-form]').removeClass('d-flex');
+      $('[id=confirm-tip-form]').addClass('d-none');
+      //Then Show the last tip form
+      if(this.get('tipToggled')){
+
+          $('[id=tip-form]').removeClass('d-none');
+          $('[id=tip-form]').addClass('d-flex');
+          $('[id=simple-tip-form]').removeClass('d-flex');
+          $('[id=simple-tip-form]').addClass('d-none');
+          $('[id=tip-toggle]').removeClass('fa-bars');
+          $('[id=tip-toggle]').addClass('fa-chevron-right');
+
+          $('[id=profileControls]').addClass('large-controls');
+      }else{
+        $('[id=tip-form]').removeClass('d-flex');
+        $('[id=tip-form]').addClass('d-none');
+        $('[id=simple-tip-form]').removeClass('d-none');
+        $('[id=simple-tip-form]').addClass('d-flex');
+
+        $('[id=tip-toggle]').removeClass('fa-chevron-right');
+        $('[id=tip-toggle]').addClass('fa-bars');
+
+        $('[id=profileControls]').removeClass('large-controls');
+      }
+    },
     sendTip(){
+
+      //Hide Confirm Tip
+
+      $('[id=confirm-tip-form]').removeClass('d-flex');
+      $('[id=confirm-tip-form]').addClass('d-none');
+      //Then Show the last tip form
+      if(this.get('tipToggled')){
+
+          $('[id=tip-form]').removeClass('d-none');
+          $('[id=tip-form]').addClass('d-flex');
+          $('[id=simple-tip-form]').removeClass('d-flex');
+          $('[id=simple-tip-form]').addClass('d-none');
+          $('[id=tip-toggle]').removeClass('fa-bars');
+          $('[id=tip-toggle]').addClass('fa-chevron-right');
+
+          $('[id=profileControls]').addClass('large-controls');
+      }else{
+        $('[id=tip-form]').removeClass('d-flex');
+        $('[id=tip-form]').addClass('d-none');
+        $('[id=simple-tip-form]').removeClass('d-none');
+        $('[id=simple-tip-form]').addClass('d-flex');
+
+        $('[id=tip-toggle]').removeClass('fa-chevron-right');
+        $('[id=tip-toggle]').addClass('fa-bars');
+
+        $('[id=profileControls]').removeClass('large-controls');
+
+      }
+
+    },
+    sendMessage(){
+
+    },
+    joinGame(){
+
+    },
+    tipToggle(){
+
+      if(this.get('tipToggled')){
+        $('[id=tip-form]').removeClass('d-flex');
+        $('[id=simple-tip-form]').removeClass('d-none');
+          $('[id=tip-form]').addClass('d-none');
+          $('[id=simple-tip-form]').addClass('d-flex');
+
+          $('[id=tip-toggle]').removeClass('fa-chevron-right');
+          $('[id=tip-toggle]').addClass('fa-bars');
+
+          $('[id=profileControls]').removeClass('large-controls');
+
+
+        this.set('tipToggled', false);
+      }else{
+        $('[id=tip-form]').removeClass('d-none');
+        $('[id=simple-tip-form]').removeClass('d-flex');
+          $('[id=tip-form]').addClass('d-flex');
+          $('[id=simple-tip-form]').addClass('d-none');
+          $('[id=tip-toggle]').removeClass('fa-bars');
+          $('[id=tip-toggle]').addClass('fa-chevron-right');
+
+          $('[id=profileControls]').addClass('large-controls');
+
+        this.set('tipToggled', true);
+      }
+
+
+    },
+    copyUrlToClipboard(){
+      /* Get the text field */
+      var copyText =document.getElementById("urlDisplay");
+
+      /* Select the text field */
+      copyText.select();
+
+      /* Copy the text inside the text field */
+      document.execCommand("Copy");
+
+      /* notify the user */
+
+      $('[id=copyUrlBtn]').removeClass('fa-link');
+      $('[id=copyUrlBtn]').addClass('fa-check');
+
+      setTimeout(function() {
+      $('[id=copyUrlBtn]').removeClass('fa-check');
+      $('[id=copyUrlBtn]').addClass('fa-link');
+      }, 3000);
+
 
     },
   }
--- app/routes/p.js
+++ app/routes/p.js
@@ -2,8 +2,14 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
 
-  model(params) {
-    return this.get('store').findRecord('user', params.profile_id);
-  }
 
+  //model(params) {
+//    return this.get('store').findRecord('user', params.profile_id);
+//  },
+ setupController(controller,model) {
+    this._super(controller, model);
+    // Set account settings to settings pulled from db
+    controller.set('tipAmountOptions', 10);
+
+  }
 });
