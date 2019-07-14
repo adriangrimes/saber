9a24d854e15cd5e304f2af74b356e2d0cf751410
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2019-07-13 21:23:56 -0700

    Modify help/index for use in database

--- app/routes/help/index.js
+++ app/routes/help/index.js
@@ -12,30 +12,22 @@ let helpTopic =[
 
    helpSections: [
           {
-            'sectionTitle' : 'Where do I sign up? This should be visible by everyone',
-            'sectionBody' : 'Right <a href="/signup">here</a> why thanks for asking!',
+            'sectionTitle' : 'Where do I sign up?',
+            'sectionBody' : 'Right <br><a href="/signup">here</a> why thanks for asking!',
             },
             {
-            'sectionTitle' : 'Second Title',
-            'sectionBody' : 'Second Body',
+            'sectionTitle' : 'How do you use my email address?',
+            'sectionBody' : 'A verifiable email address is required for signup. This will not be shown anywhere on the site, and is used solely for account management and your chosen notifications. If you created an account with an email address you do not have access to, you will not be able to make another account with that username for two days. Unconfirmed accounts will be deleted two days after their creation.',
           },
-        ]
-},
-{
-   shortTitle: 'rulesHelp',
-   title: 'Site Rules',
-   all: true,
-   isContractedOnly: false,
-   broadcastersOnly: false,
-   developersOnly: false,
-
-   helpSections: [
-            {
-            'sectionTitle' : 'NO GIRAFFES!',
-            'sectionBody' : 'I mean seriousy. ',
+          {
+          'sectionTitle' : 'Username Rules',
+          'sectionBody' : ' Usernames can be between 3 and 26 characters long, and include alphanumeric characters and hyphens (-) and underscores (_).',
           },
+          {
+          'sectionTitle' : 'Forgotten Password',
+          'sectionBody' : 'If you have forgotten your password, visit the <A TAG NEEDED>forgot your password page</a> to recover your account.',
+        },
         ]
-
 },
 {
    shortTitle: 'payoutHelp',
@@ -50,6 +42,14 @@ let helpTopic =[
             'sectionTitle' : 'This should be visible by Devs and Broadcasters',
             'sectionBody' : 'Lots of ways! Check',
             },
+            {
+            'sectionTitle' : 'Payout Requirments',
+            'sectionBody' : 'You need at least $XX.XX in your account when payments are processed to be receive payment.',
+          },
+          {
+          'sectionTitle' : 'Payroll Day',
+          'sectionBody' : 'Payments are processed on the XX and the XX of every month.',
+        },
         ]
 },
 {
@@ -61,6 +61,14 @@ let helpTopic =[
    developersOnly: true,
 
    helpSections: [
+          {
+            'sectionTitle' : 'How can I API? This should be visible by Developers',
+            'sectionBody' : 'Lots of ways! Check 123 testing',
+            },
+            {
+            'sectionTitle' : 'Game Requirments',
+            'sectionBody' : 'HTML5 or Flash pls.',
+          },
           {
           'sectionTitle' : 'Where do documentations live?',
           'sectionBody' : 'Right over here:',
@@ -80,6 +88,14 @@ let helpTopic =[
             'sectionTitle' : 'How can I RTMP? This should be visible by Broadcasters',
             'sectionBody' : 'Ya need some software m8',
             },
+            {
+            'sectionTitle' : 'But no software pls.',
+            'sectionBody' : 'Fine do a quick stream from your browser',
+          },
+          {
+          'sectionTitle' : 'What settings should I use?',
+          'sectionBody' : 'Good question',
+        },
         ]
 },
 
