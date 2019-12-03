14cacb3491bd1dc3574e76f3c160c005e663f48e
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2019-12-02 22:22:11 -0800

    Finish reskinning radio buttons/checkboxes
    Additional CSS changes.

--- ember/app/components/buy-credits.js
+++ ember/app/components/buy-credits.js
@@ -1,12 +1,30 @@
 import Component from '@ember/component';
 import { inject } from '@ember/service';
+import jQuery from 'jquery';
 
 //Component - buy-credits
 export default Component.extend({
   store: inject(),
   session: inject(),
 
+
+  package1: 100,
+  package2: 250,
+  package3: 500,
+  package4: 750,
+  package5: 1000,
+
+
   actions: {
+
+      selectAmount(amount) {
+        console.log('buy this many credits'+amount);
+        this.set('genderSelection', amount);
+        jQuery('#'+amount)
+          .prop('checked', true)
+          .change();
+      },
+
     purchaseCredits() {
       console.log('purchasing: ', this.get('creditAmount'));
 
