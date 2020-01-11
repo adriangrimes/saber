570eae7cf35fae3a24c4d4f247818da5c6ebf960
Adrian Grimes <60244190+adriangrimes@users.noreply.github.com>
2020-01-11 15:46:23 -0800

    Fix tip toolbar and add implement additional template logic
    Fixed tip toolbar item heights and text center
    Changed most programatically assigned classes to template if
    statements

--- ember/app/components/confirm-input.js
+++ ember/app/components/confirm-input.js
@@ -2,23 +2,23 @@ import Component from '@ember/component';
 import { later } from '@ember/runloop';
 
 export default Component.extend({
-  showConfirm: 'd-none',
-  showButton: 'd-flex',
+  showConfirm: false,
+  showButton: true,
   resetIcon: 'fa fa-refresh',
 
   actions: {
     openConfirm() {
       console.log(this.type + ' confirm opened');
-      this.set('showButton', 'd-none');
-      this.set('showConfirm', 'd-flex');
+      this.set('showButton', false);
+      this.set('showConfirm', true);
     },
 
     confirmInput() {
       console.log(this.type + ' input confirmed');
       this.onConfirm();
       this.set('resetIcon', 'fa fa-refresh fa-spin');
-      this.set('showButton', 'd-flex');
-      this.set('showConfirm', 'd-none');
+      this.set('showButton', true);
+      this.set('showConfirm', false);
       later(
         this,
         function() {
@@ -30,8 +30,8 @@ export default Component.extend({
 
     cancelInput() {
       console.log(this.type + ' input canceled');
-      this.set('showButton', 'd-flex');
-      this.set('showConfirm', 'd-none');
+      this.set('showButton', true);
+      this.set('showConfirm', false);
     }
   }
 });
